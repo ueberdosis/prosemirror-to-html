d8480e117cca7f64ca02cd598f9bd665daea02ec
Wiebke Vogel <wiebke.vogel@spiegel.de>
2020-01-16 11:42:36 +0100

    set attrs rel and target only if they exist

--- src/Marks/Link.php
+++ src/Marks/Link.php
@@ -11,14 +11,22 @@ class Link extends Mark
 
     public function tag()
     {
+        $attrs = [];
+
+        if (isset($this->mark->attrs->target)) {
+            $attrs['target'] = $this->mark->attrs->target;
+        }
+
+        if (isset($this->mark->attrs->rel)) {
+            $attrs['target'] = $this->nmarkde->attrs->rel;
+        }
+
+        $attrs['href'] = $this->mark->attrs->href;
+
         return [
             [
                 'tag' => 'a',
-                'attrs' => [
-                    'href' => $this->mark->attrs->href,
-                    'target' => $this->mark->attrs->target,
-                    'rel' => $this->mark->attrs->rel,
-                ],
+                'attrs' => $attrs,
             ],
         ];
     }
