8489ace7ac12a70162212d94f70cd6121e619a56
Wiebke Vogel <wiebke.vogel@spiegel.de>
2020-01-16 11:59:01 +0100

    add optional attrs target and rel to link mark

--- src/Marks/Link.php
+++ src/Marks/Link.php
@@ -11,12 +11,22 @@ class Link extends Mark
 
     public function tag()
     {
+        $attrs = [];
+
+        if (isset($this->mark->attrs->target)) {
+            $attrs['target'] = $this->mark->attrs->target;
+        }
+
+        if (isset($this->mark->attrs->rel)) {
+            $attrs['rel'] = $this->mark->attrs->rel;
+        }
+
+        $attrs['href'] = $this->mark->attrs->href;
+
         return [
             [
                 'tag' => 'a',
-                'attrs' => [
-                    'href' => $this->mark->attrs->href,
-                ],
+                'attrs' => $attrs,
             ],
         ];
     }
