81c925f548a3d8f9f3d05a88019c8e43413df775
Hans Pagel <hans.pagel@ueber.io>
2020-01-31 11:12:50 +0100

    add tests to link mark

--- tests/Marks/LinkTest.php
+++ tests/Marks/LinkTest.php
@@ -32,4 +32,58 @@ class LinkTest extends TestCase
 
         $this->assertEquals($html, (new Renderer)->render($json));
     }
+
+    /** @test */
+    public function link_mark_has_support_for_rel()
+    {
+        $json = [
+            'type' => 'doc',
+            'content' => [
+                [
+                    'type' => 'text',
+                    'text' => 'Example Link',
+                    'marks' => [
+                        [
+                            'type' => 'link',
+                            'attrs' => [
+                                'href' => 'https://scrumpy.io',
+                                'rel' => 'noopener',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $html = '<a rel="noopener" href="https://scrumpy.io">Example Link</a>';
+
+        $this->assertEquals($html, (new Renderer)->render($json));
+    }
+
+    /** @test */
+    public function link_mark_has_support_for_target()
+    {
+        $json = [
+            'type' => 'doc',
+            'content' => [
+                [
+                    'type' => 'text',
+                    'text' => 'Example Link',
+                    'marks' => [
+                        [
+                            'type' => 'link',
+                            'attrs' => [
+                                'href' => 'https://scrumpy.io',
+                                'target' => '_blank',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $html = '<a target="_blank" href="https://scrumpy.io">Example Link</a>';
+
+        $this->assertEquals($html, (new Renderer)->render($json));
+    }
 }
